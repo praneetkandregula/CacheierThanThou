c751623906253d3c9f7f399e9a215f156c5f6fd4
praneetkandregula <praneetkandregula123@gmail.com>
2025-06-06 22:32:23 +0530

    Added main application

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/raghavroy145/DistributedCaching/cache"
+	"github.com/raghavroy145/DistributedCaching/client"
+)
+
+func main() {
+	var (
+		listenAddr = flag.String("listenaddr", ":3000", "listen address of the server")
+		leaderAddr = flag.String("leaderaddr", "", "listen address of the leader")
+	)
+	flag.Parse()
+
+	opts := ServerOpts{
+		ListenAddr: *listenAddr,
+		IsLeader:   len(*leaderAddr) == 0,
+		LeaderAddr: *leaderAddr,
+	}
+	go func() {
+		time.Sleep(time.Second * 5)
+		if opts.IsLeader {
+			SendStuff()
+		}
+	}()
+
+	server := NewServer(opts, cache.New())
+	server.Start()
+}
+
+func SendStuff() {
+	for i := 0; i < 200; i++ {
+		go func(i int) {
+			client, err := client.New(":3000", client.Options{})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var (
+				key   = []byte(fmt.Sprintf("key_%d", i))
+				value = []byte(fmt.Sprintf("val_%d", i))
+			)
+
+			if err := client.Set(context.Background(), key, value, 0); err != nil {
+				log.Fatal(err)
+			}
+
+			fetchedValue, err := client.Get(context.Background(), key)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(string(fetchedValue))
+			client.Close()
+		}(i)
+	}
+}
